56447a481c0a34a37232a9a430d2bd2a30e7a54a
nananapo <kanapodream@gmail.com>
2023-01-13 21:41:19 +0900

    16進数表記に変更

--- src/memory.v
+++ src/memory.v
@@ -9,20 +9,20 @@ module Memory #(
 reg [7:0] mem [MEMORY_SIZE-1:0];
 
 initial begin
-    mem[0] = 11;
-    mem[1] = 12;
-    mem[2] = 13;
-    mem[3] = 14;
+    mem[0] = 8'h11;
+    mem[1] = 8'h12;
+    mem[2] = 8'h13;
+    mem[3] = 8'h14;
 
-    mem[4] = 21;
-    mem[5] = 22;
-    mem[6] = 23;
-    mem[7] = 24;
+    mem[4] = 8'h21;
+    mem[5] = 8'h22;
+    mem[6] = 8'h23;
+    mem[7] = 8'h24;
 
-    mem[8] = 31;
-    mem[9] = 32;
-    mem[10] = 33;
-    mem[11] = 34;
+    mem[8] = 8'h31;
+    mem[9] = 8'h32;
+    mem[10] = 8'h33;
+    mem[11] = 8'h34;
 end
 
 assign inst = {mem[addr + 3], mem[addr + 2], mem[addr + 1], mem[addr]};
