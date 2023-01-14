a8f2b77609711808dbdcf358711bec6b5375fe24
nananapo <kanapodream@gmail.com>
2023-01-14 18:55:42 +0900

    テストが通ったかどうかを出力するようにした

--- src/test.v
+++ src/test.v
@@ -13,16 +13,24 @@ module test();
 	.gp(gp)
   );
 
+  reg [63:0] testclk = 0;
+
   always
     #1 clk = ~clk;
 
-  initial begin
-    #100000 $finish;
-  end
-
   always @(posedge clk) begin
-	if (exit)
+	if (exit) begin
+		if (gp == 1)
+			$display("Test passed");
+		else
+			$display("Test failed : gp(%d) is not 1", gp);
     	$finish;
+	end else begin
+		if (testclk > 100000) begin
+			$display("Test failed : Max clock exceeded");
+			$finish;
+		end
+	end
   end
 
   initial begin
