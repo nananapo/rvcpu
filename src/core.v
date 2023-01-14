7542edef57189d684bb96e64a90291c68b0e45e3
nananapo <kanapodream@gmail.com>
2023-01-14 11:07:56 +0900

    りふぁくた

--- src/core.v
+++ src/core.v
@@ -97,12 +97,11 @@ wire [WORD_LEN-1:0] wb_data = (
 // 終了判定
 assign exit = memory_i_addr == 8;
 
-integer loop_i;
 always @(negedge rst_n or posedge clk) begin
     if (!rst_n) begin
         reg_pc <= 0;
-        for (loop_i = 0; loop_i < REGISTER_COUNT; loop_i = loop_i + 1)
-            regfile[loop_i] <= 0;
+        for (loop_initial_regfile_i = 0; loop_initial_regfile_i < REGISTER_COUNT; loop_initial_regfile_i = loop_initial_regfile_i + 1)
+            regfile[loop_initial_regfile_i] <= 0;
     end else if (!exit) begin
         reg_pc <= reg_pc + 4;
 
