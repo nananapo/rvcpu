a33659fa7d8f2f716df81ccbc17233ee854178ff
nananapo <kanapodream@gmail.com>
2023-01-14 18:01:05 +0900

    exitか100000クロック目で終了するようにテストを変更

--- src/test.v
+++ src/test.v
@@ -2,19 +2,27 @@ module test();
   reg rst_n = 1;
   reg clk = 0;
   wire [5:0] led;
+  wire exit;
+  wire [31:0] gp;
 
   main #() m(
     .rst_n(rst_n),
     .clk(clk),
-    .led(led)
+    .led(led),
+	.exit(exit),
+	.gp(gp)
   );
 
   always
     #1 clk = ~clk;
 
   initial begin
-    $display("Starting");
-    #1000 $finish;
+    #100000 $finish;
+  end
+
+  always @(posedge clk) begin
+	if (exit)
+    	$finish;
   end
 
   initial begin
