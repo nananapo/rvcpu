d993fcf73c6c5abc48e0005aca468d56aa0ab103
na <kanapodream@gmail.com>
2023-01-15 11:41:12 +0900

    ワード単位でメモリの読み書きを行うようにした

--- src/memory.v
+++ src/memory.v
@@ -4,29 +4,28 @@ module Memory #(
 ) (
     input wire clk,
     input wire [WORD_LEN-1:0] i_addr,
-    output wire [WORD_LEN-1:0] inst,
+    output reg [WORD_LEN-1:0] inst,
     input wire [WORD_LEN-1:0] d_addr,
-    output wire [WORD_LEN-1:0] rdata,
+    output reg [WORD_LEN-1:0] rdata,
     input wire wen,
     input wire [WORD_LEN-1:0] wdata
 );
 
-reg [7:0] mem [MEMORY_SIZE-1:0];
+reg [WORD_LEN-1:0] mem [(MEMORY_SIZE >> 2) - 1:0];
 
 initial begin
     $readmemh("../test/bin/rv32ui-p-add.hex", mem);
 end
 
-assign inst = {mem[i_addr + 3], mem[i_addr + 2], mem[i_addr + 1], mem[i_addr]};
-assign rdata = {mem[d_addr + 3], mem[d_addr + 2], mem[d_addr + 1], mem[d_addr]};
+wire [13:0] i_addr_mod = (i_addr % MEMORY_SIZE) >> 2;
+wire [13:0] d_addr_mod = (d_addr % MEMORY_SIZE) >> 2;
 
 always @(posedge clk) begin
     if (wen) begin
-        mem[d_addr] <= wdata[7:0];
-        mem[d_addr + 1] <= wdata[15:8];
-        mem[d_addr + 2] <= wdata[23:16];
-        mem[d_addr + 3] <= wdata[31:24];
+        mem[d_addr] <= {wdata[7:0], wdata[15:8], wdata[23:16], wdata[31:24]};
     end
+    inst <= {mem[i_addr_mod][7:0], mem[i_addr_mod][15:8], mem[i_addr_mod][23:16], mem[i_addr_mod][31:24]};
+    rdata <= {mem[d_addr_mod][7:0], mem[d_addr_mod][15:8], mem[d_addr_mod][23:16], mem[d_addr_mod][31:24]};
 end
 
 endmodule
