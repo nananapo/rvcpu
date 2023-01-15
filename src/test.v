d07e7321b4bdde95d1e02a7a17b86a64b3924d72
nananapo <kanapodream@gmail.com>
2023-01-15 12:54:48 +0900

    testでledは使わないので削除

--- src/test.v
+++ src/test.v
@@ -1,16 +1,14 @@
 module test();
   reg rst_n = 1;
   reg clk = 0;
-  wire [5:0] led;
   wire exit;
   wire [31:0] gp;
 
   main #() m(
     .rst_n(rst_n),
     .clk(clk),
-    .led(led),
-	.exit(exit),
-	.gp(gp)
+	  .exit(exit),
+	  .gp(gp)
   );
 
   reg [63:0] testclk = 0;
