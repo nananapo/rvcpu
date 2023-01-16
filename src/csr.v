4ddf3c8e93990dfcb1b6097875701ffc67728b22
nananapo <kanapodream@gmail.com>
2023-01-16 09:42:28 +0900

    csrがwenのときにアドレスと値を表示するようにした

--- src/csr.v
+++ src/csr.v
@@ -23,7 +23,7 @@ always @(posedge clk) begin
 	rdata <= {mem[addr][7:0], mem[addr][15:8], mem[addr][23:16], mem[addr][31:24]};
 	trap_vector <= {mem[TV_ADDR][7:0], mem[TV_ADDR][15:8], mem[TV_ADDR][23:16], mem[TV_ADDR][31:24]};
 	if (wen) begin
-        $display("csr wen : 1;");
+        $display("csr wen %h:%h", addr, wdata);
 		mem[addr] <= {wdata[7:0], wdata[15:8], wdata[23:16], wdata[31:24]};
 	end
 end
