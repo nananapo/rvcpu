cf4e10a23449b79d78f82d0d583d04675da31afc
na <kanapodream@gmail.com>
2023-01-20 21:29:44 +0900

    UARTのテストプログラムを更新
    データの配置場所を明示したよ

--- test/c/uart_tx.c
+++ test/c/uart_tx.c
@@ -1,17 +1,30 @@
-char *str = "Hello World!";
-int strsize = 12;
+char *str = (void *)10000;
+#define STRSIZE 12;
 
 int main(void)
 {
+	str[0] = "H";
+	str[1] = "e";
+	str[2] = "l";
+	str[3] = "l";
+	str[4] = "o";
+	str[5] = " ";
+	str[6] = "W";
+	str[7] = "o";
+	str[8] = "r";
+	str[9] = "l";
+	str[10]= "d";
+	str[11]= "!";
+
 	int *tailptr = (void *)10240;
 	int *dataptr = (void *)10241;
 
-	for (int i = 0; i < strsize; i++)
+	for (int i = 0; i < STRSIZE; i++)
 	{
 		int dindex = (*tailptr + i) % 32;
 		dataptr[dindex] = str[i];
 	}
-	*tailptr = (*tailptr + strsize) % 32;
+	*tailptr = (*tailptr + STRSIZE) % 32;
 
 	while (1);
 }
