83cb47cb3794dc6b402a3e56c3039b414f6c141f
na <kanapodream@gmail.com>
2023-02-23 06:02:03 +0900

    メモリステージがストール中、実行ステージもストールさせる

--- src/pipeline/executestage.v
+++ src/pipeline/executestage.v
@@ -2,15 +2,14 @@ module ExecuteStage
 (
 	input wire clk,
 
-	input reg [31:0] imm_b_sext,
-
-	input reg [31:0] reg_pc,
-	input reg [4:0]  exe_fun,
-	input reg [31:0] op1_data,
-	input reg [31:0] op2_data,
-	input reg [31:0] rs2_data,
-	input reg [4:0]  mem_wen,
-	input reg [3:0]  wb_sel,
+	input reg [31:0] input_reg_pc,
+	input reg [4:0]  input_exe_fun,
+	input reg [31:0] input_op1_data,
+	input reg [31:0] input_op2_data,
+	input reg [31:0] input_rs2_data,
+	input reg [4:0]  input_mem_wen,
+	input reg [3:0]  input_wb_sel,
+	input reg [31:0] input_imm_b_sext,
 
 	output reg [31:0] alu_out,
 	output reg        br_flg,
@@ -19,10 +18,30 @@ module ExecuteStage
 	output reg [31:0] output_reg_pc,
 	output reg [4:0]  output_mem_wen,
 	output reg [3:0]  output_wb_sel,
-	output reg [31:0] output_rs2_data
+	output reg [31:0] output_rs2_data,
+	
+	input  wire       stall_flg
 );
 
 `include "../consts_core.v"
+
+reg [31:0] save_reg_pc = 0;	
+reg [4:0]  save_exe_fun = 0;	
+reg [31:0] save_op1_data = 0;
+reg [31:0] save_op2_data = 0;
+reg [31:0] save_rs2_data = 0;
+reg [4:0]  save_mem_wen = 0;	
+reg [3:0]  save_wb_sel = 0;	
+reg [31:0] save_imm_b_sext = 0;
+
+wire [31:0] reg_pc		= stall_flg ? save_reg_pc : input_reg_pc;
+wire [4:0]  exe_fun		= stall_flg ? save_exe_fun : input_exe_fun;
+wire [31:0] op1_data	= stall_flg ? save_op1_data : input_op1_data;
+wire [31:0] op2_data	= stall_flg ? save_op2_data : input_op2_data;
+wire [31:0] rs2_data	= stall_flg ? save_rs2_data : input_rs2_data;
+wire [4:0]  mem_wen		= stall_flg ? save_mem_wen : input_mem_wen;
+wire [3:0]  wb_sel		= stall_flg ? save_wb_sel : input_wb_sel;
+wire [31:0] imm_b_sext	= stall_flg ? save_imm_b_sext : input_imm_b_sext;
 
 always @(posedge clk) begin
 	// EX STAGE
@@ -59,6 +78,18 @@ always @(posedge clk) begin
 	output_wb_sel   <= wb_sel;
 	output_rs2_data <= rs2_data;
 
+	// save
+	save_reg_pc		<= reg_pc;	
+	save_exe_fun	<= exe_fun;	
+	save_op1_data	<= op1_data;
+	save_op2_data	<= op2_data;
+	save_rs2_data	<= rs2_data;
+	save_mem_wen	<= mem_wen;	
+	save_wb_sel		<= wb_sel;	
+	save_imm_b_sext	<= imm_b_sext;
+end
+
+always @(posedge clk) begin
 	$display("EXECUTE -------------");
     $display("exe_fun   : %d", exe_fun);
     $display("op1_data  : 0x%H", op1_data);
