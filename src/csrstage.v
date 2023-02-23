b4f9585672ac44e317cebedca3748f8184b42cff
na <kanapodream@gmail.com>
2023-02-24 03:56:52 +0900

    CSRStageの実装

--- src/csrstage.v
+++ src/csrstage.v
@@ -1,36 +1,51 @@
-module CSRStage(
-	input wire clk
-	output reg [WORD_LEN-1:0]		trap_vector
+module CSRStage #(
+	parameter TV_ADDR = 12'h305
+)(
+	input wire clk,
+	
+	// input
+	input  wire [2:0]	csr_cmd,
+	input  wire [31:0]  op1_data,
+	input  wire [31:0]	imm_i,
+
+	// output
+	output wire [31:0]	trap_vector
 );
 
-// CSR
-reg csr_clock = 0;  // CSR命令の時にレジスタ読み出しを待機するためのフラグ
+`include "consts_core.v"
 
-wire [11:0] csr_addr = (
-	csr_cmd == CSR_E ? 12'h342 : 
-	imm_i
-);
+reg [31:0] mem [4095:0];
 
-reg csr_wen = 0;
-wire [WORD_LEN-1:0] csr_rdata;
-wire [WORD_LEN-1:0] trap_vector_addr;
-wire [WORD_LEN-1:0] csr_wdata = (
+// trap先
+assign trap_vector = {mem[TV_ADDR][7:0], mem[TV_ADDR][15:8], mem[TV_ADDR][23:16], mem[TV_ADDR][31:24]};
+
+// ecallなら0x342を読む
+wire [11:0] addr	= csr_cmd == CSR_E ? 12'h342 : imm_i;
+wire [31:0] rdata	= {mem[addr][7:0], mem[addr][15:8], mem[addr][23:16], mem[addr][31:24]};
+
+wire [31:0] wdata = (
 	csr_cmd == CSR_W ? op1_data :
-	csr_cmd == CSR_S ? csr_rdata | op1_data :
-	csr_cmd == CSR_C ? csr_rdata & ~op1_data :
-	csr_cmd == CSR_E ? 11 : // 11はマシンモードからのecallであることを示す
+	csr_cmd == CSR_S ? rdata | op1_data :
+	csr_cmd == CSR_C ? rdata & ~op1_data :
+	csr_cmd == CSR_E ? 11 :
 	0
 );
 
-Csr csr (
-	.clk(clk),
-	.addr(csr_addr),
-	.rdata(csr_rdata),
-	.wen(csr_wen),
-	.wdata(csr_wdata),
-	.trap_vector(trap_vector_addr)
-);
+always @(posedge clk) begin
+	if (csr_cmd != CSR_X) begin
+		mem[addr] <= {wdata[7:0], wdata[15:8], wdata[23:16], wdata[31:24]};
+	end
+end
 
-reg [WORD_LEN-1:0] mem [REG_SIZE-1:0];
+always @(posedge clk) begin
+	$display("CSR STAGE------------");
+	$display("cmd          : %d", csr_cmd);
+	$display("op1_data     : %d", op1_data);
+	$display("imm_i        : 0x%H", imm_i);
+	$display("addr         : 0x%H", addr);
+	$display("rdata        : 0x%H", rdata);
+	$display("wdata        : 0x%H", wdata);
+	$display("trap_vector  : 0x%H", trap_vector);
+end
 
 endmodule
