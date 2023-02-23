a1c11933664a811e17bf5833414d99e31b743d50
na <kanapodream@gmail.com>
2023-02-23 19:19:04 +0900

    rdataは常に更新しますよ

--- src/memory.v
+++ src/memory.v
@@ -22,30 +22,29 @@ initial begin
 	if (MEMORY_FILE != "") begin
 		$readmemh(MEMORY_FILE, mem);
 	end
+	rdata <= 0;
 end
 
-wire [31:0] addr_shift = addr >> 2;
+wire [31:0] addr_shift = (addr >> 2) % MEMORY_SIZE;
 
 assign cmd_ready = 1;
 assign rdata_valid = !cmd_write;
 
 always @(posedge clk) begin
-    if (cmd_start) begin
-        if (cmd_write) begin
-            mem[addr_shift] = {
-				wdata[7:0],
-				wdata[15:8],
-				wdata[23:16],
-				wdata[31:24]
-            };
-        end else begin
-            rdata <= {
-                mem[addr_shift][7:0],
-                mem[addr_shift][15:8],
-                mem[addr_shift][23:16],
-                mem[addr_shift][31:24]
-            };
-        end
+	rdata <= {
+	    mem[addr_shift][7:0],
+	    mem[addr_shift][15:8],
+	    mem[addr_shift][23:16],
+	    mem[addr_shift][31:24]
+	};
+
+    if (cmd_start && cmd_write) begin
+        mem[addr_shift] = {
+			wdata[7:0],
+			wdata[15:8],
+			wdata[23:16],
+			wdata[31:24]
+        };
     end
 end
 endmodule
