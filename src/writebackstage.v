11bb7732cfb9619557cfe5fcc0bc85c5ef99dea4
na <kanapodream@gmail.com>
2023-02-24 04:22:56 +0900

    inputとoutputだけ

--- src/writebackstage.v
+++ src/writebackstage.v
@@ -1,14 +1,29 @@
 module WriteBackStage(
-	wire input clk
+	input  wire 		clk,
+
+	input  wire [31:0]	reg_pc,
+	input  wire [3:0]	wb_sel,
+	input  wire [31:0]	csr_rdata,
+	input  wire [31:0]	memory_read,
+	input  wire [4:0]	wb_addr,
+	input  wire 		rf_wen,
+	input  wire [31:0]	br_target,
+	input  wire [31:0]	alu_out,
+	input  wire			inst_is_ecall,
+	input  wire [31:0]	trap_vector_addr,
+
+	output reg [31:0]	regfile[31:0]
 );
 
+`include "consts_core.v"
+
 // WB STAGE
-wire [WORD_LEN-1:0] wb_data = (
-	wb_sel == WB_MEMB ? {{24{memory_b_read[7]}}, memory_b_read} :
-	wb_sel == WB_MEMBU? {24'b0, memory_b_read} :
-	wb_sel == WB_MEMH ? {{16{memory_h_read[15]}}, memory_h_read} :
-	wb_sel == WB_MEMHU? {16'b0, memory_h_read} :
-	wb_sel == WB_MEMW ? memory_w_read :
+wire [31:0] wb_data = (
+	wb_sel == WB_MEMB ? {{24{memory_read[7]}}, memory_read} :
+	wb_sel == WB_MEMBU? {24'b0, memory_read} :
+	wb_sel == WB_MEMH ? {{16{memory_read[15]}}, memory_read} :
+	wb_sel == WB_MEMHU? {16'b0, memory_read} :
+	wb_sel == WB_MEMW ? memory_read :
 	wb_sel == WB_PC   ? reg_pc_plus4 :
 	wb_sel == WB_CSR  ? csr_rdata :
 	alu_out
