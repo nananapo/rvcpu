a8ea28fc04c941b39922591d15ea49baf9a6ac72
na <kanapodream@gmail.com>
2023-02-24 05:21:04 +0900

    お名前を変える

--- src/writebackstage.v
+++ src/writebackstage.v
@@ -6,13 +6,13 @@ module WriteBackStage(
 	input  wire [31:0]	reg_pc,
 	input  wire [3:0]	wb_sel,
 	input  wire [31:0]	csr_rdata,
-	input  wire [31:0]	memory_read,
+	input  wire [31:0]	memory_rdata,
 	input  wire [4:0]	wb_addr,
 	input  wire 		rf_wen,
 	input  wire [31:0]	br_target,
 	input  wire [31:0]	alu_out,
 	input  wire			inst_is_ecall,
-	input  wire [31:0]	trap_vector_addr,
+	input  wire [31:0]	trap_vector,
 
 	output reg [31:0]	regfile[31:0]
 );
@@ -30,11 +30,11 @@ end
 
 // WB STAGE
 wire [31:0] wb_data = (
-	wb_sel == WB_MEMB ? {{24{memory_read[7]}}, memory_read} :
-	wb_sel == WB_MEMBU? {24'b0, memory_read} :
-	wb_sel == WB_MEMH ? {{16{memory_read[15]}}, memory_read} :
-	wb_sel == WB_MEMHU? {16'b0, memory_read} :
-	wb_sel == WB_MEMW ? memory_read :
+	wb_sel == WB_MEMB ? {{24{memory_rdata[7]}}, memory_rdata} :
+	wb_sel == WB_MEMBU? {24'b0, memory_rdata} :
+	wb_sel == WB_MEMH ? {{16{memory_rdata[15]}}, memory_rdata} :
+	wb_sel == WB_MEMHU? {16'b0, memory_rdata} :
+	wb_sel == WB_MEMW ? memory_rdata :
 	wb_sel == WB_PC   ? reg_pc_plus4 :
 	wb_sel == WB_CSR  ? csr_rdata :
 	alu_out
@@ -47,7 +47,7 @@ always @(posedge clk) begin
 	reg_pc <= (
 		br_flg ? br_target : 
 		jmp_flg ? alu_out :
-		inst_is_ecall ? trap_vector_addr :
+		inst_is_ecall ? trap_vector :
 		reg_pc_plus4
 	);
 end
