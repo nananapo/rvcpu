77bf4778dde8a8014ee43b02304a338d8e31f2e1
na <kanapodream@gmail.com>
2023-02-28 16:55:16 +0900

    フェッチステージの分岐ハザード対応
    だが、うまく動いていないぞ

--- src/fetchstage.v
+++ src/fetchstage.v
@@ -43,27 +43,35 @@ assign mem_start = (
 );
 
 assign mem_addr = (
-    state == STATE_WAIT_READY ? reg_pc :
+    state == STATE_WAIT_READY ? (
+        wb_branch_hazard ? wb_reg_pc : inner_reg_pc
+    ) :
     state == STATE_WAIT_VALID ? (
         (!is_fetched && mem_data_valid) ? (
-            stall_flg ? REGPC_NOP : reg_pc + 4
+            stall_flg ? REGPC_NOP : (
+                wb_branch_hazard ? wb_reg_pc : inner_reg_pc + 4
+            )
         ) :
         is_fetched ? (
-            !stall_flg ? reg_pc : REGPC_NOP
+            !stall_flg ? (
+                wb_branch_hazard ? wb_reg_pc : inner_reg_pc
+            ) : REGPC_NOP
         ) : REGPC_NOP
     ) : REGPC_NOP
 );
 
 assign id_reg_pc = (
     stall_flg ? REGPC_NOP :
+    wb_branch_hazard ? REGPC_NOP :
     state == STATE_WAIT_VALID ? (
-        (!is_fetched && mem_data_valid) ? reg_pc :
+        (!is_fetched && mem_data_valid) ? inner_reg_pc :
         is_fetched ? saved_reg_pc : REGPC_NOP
     ) : REGPC_NOP
 );
 
 assign id_inst = (
     stall_flg ? INST_NOP :
+    wb_branch_hazard ? INST_NOP :
     state == STATE_WAIT_VALID ? (
         (!is_fetched && mem_data_valid) ? mem_data :
         is_fetched ? saved_inst : INST_NOP
@@ -73,36 +81,37 @@ assign id_inst = (
 always @(posedge clk) begin
     if (state == STATE_WAIT_READY) begin
         if (mem_ready) begin
-            saved_reg_pc    <= REGPC_NOP;
-            saved_inst      <= INST_NOP;
             state           <= STATE_WAIT_READY;
             is_fetched      <= 0;
         end
     end else if (state == STATE_WAIT_VALID) begin
-        if (!is_fetched && mem_data_valid) begin
-            saved_reg_pc    <= reg_pc;
-            saved_inst      <= mem_data;
-            reg_pc          <= reg_pc + 4;
-            is_fetched      <= 1;
-            $display("Fetched");
-            if (stall_flg)
-                state       <= STATE_WAIT_READY;
-            else begin
-                if (mem_ready) begin
-                    saved_reg_pc    <= REGPC_NOP;
-                    saved_inst      <= INST_NOP;
-                    is_fetched      <= 0;
-                end else
-                    state <= STATE_WAIT_READY;
-            end
-        end else if (is_fetched) begin
-            if (!stall_flg) begin
-                if (mem_ready) begin
-                    saved_reg_pc    <= REGPC_NOP;
-                    saved_inst      <= INST_NOP;
-                    is_fetched      <= 0;
-                end else
-                    state <= STATE_WAIT_READY;
+        if (wb_branch_hazard) begin
+            inner_reg_pc <= wb_reg_pc;
+            if (mem_ready) begin
+                is_fetched <= 0;
+            end else
+                state <= STATE_WAIT_READY;
+        end else begin
+            if (!is_fetched && mem_data_valid) begin
+                $display("Fetched");
+                inner_reg_pc <= inner_reg_pc + 4;
+                if (stall_flg) begin
+                    saved_reg_pc    <= inner_reg_pc;
+                    saved_inst      <= mem_data;
+                    is_fetched      <= 1;
+                end else begin
+                    if (mem_ready) begin
+                        is_fetched <= 0;
+                    end else
+                        state <= STATE_WAIT_READY;
+                end
+            end else if (is_fetched) begin
+                if (!stall_flg) begin
+                    if (mem_ready) begin
+                        is_fetched <= 0;
+                    end else
+                        state <= STATE_WAIT_READY;
+                end
             end
         end
     end
@@ -110,8 +119,8 @@ end
 
 always @(posedge clk) begin
 	$display("FETCH -------------");
-	$display("status    : %d", status);
-	$display("reg_pc    : 0x%H", reg_pc);
+	$display("status    : %d", state);
+	$display("reg_pc    : 0x%H", inner_reg_pc);
 	$display("id.reg_pc : 0x%H", id_reg_pc);
 	$display("id.inst   : 0x%H", id_inst);
 	$display("mem.start : %d", mem_start);
