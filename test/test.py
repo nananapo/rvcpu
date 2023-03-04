ac1c9c8776e7a12cac23db972db59ed3e038cd7e
na <kanapodream@gmail.com>
2023-03-05 00:58:46 +0900

    エンコードの指定が足りない！

--- test/test.py
+++ test/test.py
@@ -13,13 +13,13 @@ resultstatus = []
 def test(filename):
     # replace
     memory_v_test = memory_backup.replace(REPLACE_WORD, filename)
-    with open(MEMORY_V_FILENAME, "w") as f:
+    with open(MEMORY_V_FILENAME, "w", encoding='utf-8') as f:
         f.write(memory_v_test)
     # run test
     resultFileName = "../test/results/" + filename.replace("/","_") + ".txt"
     system("cd ../src/ && pwd && make d > " + resultFileName)
 
-    with open(resultFileName, "r") as f:
+    with open(resultFileName, "r", encoding='utf-8') as f:
         result = "".join(f.readlines())
         if "Test passed" in result:
             results.append("PASS : "+ filename)
@@ -34,15 +34,16 @@ while True:
     except Exception as e:
         if type(e) is EOFError:
             break
-        print(e.message)
+        print("Error occured", type(e))
+        #print(e.message)
         break
 
 results = sorted(results)
 
-with open("results/result.txt", "w") as f:
+with open("results/result.txt", "w", encoding='utf-8') as f:
     f.write("STATUS : " + str(sum(resultstatus)) + " / " + str(len(resultstatus)) + "\n")
     f.write("\n".join(results))
 
 # rollback
-with open(MEMORY_V_FILENAME, "w") as f:
+with open(MEMORY_V_FILENAME, "w", encoding='utf-8') as f:
     f.write(memory_backup)
