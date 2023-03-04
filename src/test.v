0ab69d2573a06cc1e0a897096dfb819fa962ec88
na <kanapodream@gmail.com>
2023-03-04 20:49:42 +0900

    testでexitとgpを見るよ

--- src/test.v
+++ src/test.v
@@ -2,15 +2,29 @@
 
 module test();
   reg clk = 1;
+  reg exit;
+  reg [31:0] gp;
 
   main #() m(
-    .clk(clk)
+    .clk(clk),
+    .exit(exit),
+    .gp(gp)
   );
 
   always
     #1 clk = ~clk;
 
   initial
-    #10001 $finish;
+    #100001 $finish;
+
+  always @(posedge clk) begin
+	if (exit) begin
+		if (gp == 1)
+			$display("Test passed");
+		else
+			$display("Test failed : gp(%d) is not 1", gp);
+    	$finish;
+	end 
+  end
 
 endmodule
