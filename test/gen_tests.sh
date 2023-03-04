1eb9045258211061fbf16098b4a191893fe13d52
na <kanapodream@gmail.com>
2023-03-05 00:42:36 +0900

    コマンドをつなげるよ

--- test/gen_tests.sh
+++ test/gen_tests.sh
@@ -1,2 +1,1 @@
-find riscv-tests -type file ! -name "*\.*" -exec sh -c "riscv64-unknown-elf-objcopy -O binary {} {}.bin" \;
-find riscv-tests -type file ! -name "*\.*" -exec sh -c "echo \"{}.bin\" | python3 bin2hex.py" \;
+find riscv-tests -type f ! -name "*\.*" -exec sh -c "riscv64-unknown-elf-objcopy -O binary {} {}.bin" \; && find riscv-tests -type f ! -name "*\.*" -exec sh -c "echo \"{}.bin\" | python3 bin2hex.py" \;
