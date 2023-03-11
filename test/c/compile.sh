00bea20a1025a8f7414ea9e1a6be55d5c3a07e51
na <kanapodream@gmail.com>
2023-03-12 04:59:45 +0900

    パスを追加するコマンドを記述

--- test/c/compile.sh
+++ test/c/compile.sh
@@ -1,5 +1,5 @@
 FILE=$1
-
+#PATH=$PATH:/opt/riscv32/bin/
 riscv32-unknown-elf-gcc -march=rv32i -c -o temp.o $FILE
 riscv32-unknown-elf-ld temp.o -lc -L/opt/riscv32/riscv32-unknown-elf/lib/ -Tlink.ld -nostartfiles -static -o temp.elf
 riscv32-unknown-elf-objcopy -O binary temp.elf temp.bin
