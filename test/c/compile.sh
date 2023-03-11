25b79902260687c4f0c57322021bdd90fbdabaf3
na <kanapodream@gmail.com>
2023-03-12 07:17:42 +0900

    最適化の無効化

--- test/c/compile.sh
+++ test/c/compile.sh
@@ -1,6 +1,6 @@
 FILE=$1
 #PATH=$PATH:/opt/riscv32/bin/
-riscv32-unknown-elf-gcc -march=rv32i -c -o temp.o $FILE
+riscv32-unknown-elf-gcc -march=rv32i -O0 -c -o temp.o $FILE
 riscv32-unknown-elf-ld temp.o -lc -L/opt/riscv32/riscv32-unknown-elf/lib/ -Tlink.ld -nostartfiles -static -o temp.elf
 riscv32-unknown-elf-objcopy -O binary temp.elf temp.bin
 echo "temp.bin" | python ../bin2hex.py
