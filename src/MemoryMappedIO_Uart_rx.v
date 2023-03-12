4a66176342d7a6b130aa89eda82691f8a24173f0
na <kanapodream@gmail.com>
2023-03-13 03:11:32 +0900

    バッファのデータを正しく読み込めていなかったのを修正

--- src/MemoryMappedIO_Uart_rx.v
+++ src/MemoryMappedIO_Uart_rx.v
@@ -15,9 +15,10 @@ module MemoryMappedIO_Uart_rx
 
 `include "include/memorymap.v"
 
-localparam STATE_IDLE               = 0;
-localparam STATE_WRITE_MEM          = 1;
-localparam STATE_READBUF_AFTER_RX   = 2;
+localparam STATE_IDLE                       = 0;
+localparam STATE_WRITE_MEM                  = 1;
+localparam STATE_READBUF_AFTER_RX           = 2;
+localparam STATE_READBUF_BEFORE_WRITE_MEM   = 3;
 
 // メモリ
 reg [31:0] buffer[255:0];
@@ -46,22 +47,23 @@ Uart_rx #() rxModule(
 );
 
 // アドレスは4byteずつ
-function [7:0] func_addr(
+function [7:0] func_read_addr(
     input [1:0] state,
     input       rx_rdata_valid,
     input [9:0] buffer_tail,
     input [31:0]input_addr
 );
     case (state)
-        STATE_IDLE              : func_addr = rx_rdata_valid ? buffer_tail[9:2] : input_addr[9:2];
-        STATE_WRITE_MEM         : func_addr = buffer_tail[9:2];
-        STATE_READBUF_AFTER_RX  : func_addr = input_addr[9:2];
-        default                 : func_addr = 0;
+        STATE_IDLE                      : func_read_addr = rx_rdata_valid ? buffer_tail[9:2] : input_addr[9:2];
+        STATE_WRITE_MEM                 : func_read_addr = input_addr[9:2];
+        STATE_READBUF_AFTER_RX          : func_read_addr = input_addr[9:2];
+        STATE_READBUF_BEFORE_WRITE_MEM  : func_read_addr = buffer_tail[9:2];
+        default                         : func_read_addr = 0;
     endcase
 endfunction
 
-wire [7:0] addr     = func_addr(state, rx_rdata_valid, buffer_tail, input_addr);
-wire [1:0] addr_mod = buffer_tail[1:0]; // buffer_tail % 4
+wire [7:0] read_addr    = func_read_addr(state, rx_rdata_valid, buffer_tail, input_addr);
+wire [7:0] write_addr   = buffer_tail[9:2];
 
 // 状態
 reg [1:0] state = STATE_IDLE;
@@ -83,10 +85,14 @@ function [31:0] func_buf_wdata(
         3: func_buf_wdata = {uart_rdata, buf_rdata[23:0]};
     endcase
 endfunction
-wire [31:0] buf_wdata = func_buf_wdata(buf_rdata, uart_rdata, addr_mod);
+wire [31:0] buf_wdata = func_buf_wdata(buf_rdata, uart_rdata, buffer_tail[1:0]);
 
 assign output_cmd_ready     = state == STATE_IDLE;
-assign output_rdata_valid   = state == STATE_IDLE;
+assign output_rdata_valid   = is_tail_addr || is_count_addr || (
+    is_buffer_addr ?
+        state == STATE_READBUF_BEFORE_WRITE_MEM || state == STATE_IDLE
+        : 0
+);
 
 reg save_input_cmd_start = 0;
 reg save_input_cmd_write = 0;
@@ -98,9 +104,9 @@ always @(posedge clk) begin
     else if (is_count_addr)
         output_rdata <= buffer_count;
     else if (is_buffer_addr)
-        output_rdata <= buffer[addr];
+        output_rdata <= buf_rdata;
 
-    buf_rdata            <= buffer[addr];
+    buf_rdata            <= buffer[read_addr];
     save_input_cmd_start <= input_cmd_start;
     save_input_cmd_write <= input_cmd_write;
 
@@ -109,11 +115,13 @@ always @(posedge clk) begin
             if (rx_rdata_valid) begin
                 state       <= STATE_WRITE_MEM;
                 uart_rdata  <= rx_rdata;
+            end else if (input_cmd_start) begin
+                state       <= STATE_READBUF_AFTER_RX;
             end
         end
         STATE_WRITE_MEM: begin
-            buffer[addr]    <= buf_wdata;
-            buffer_tail     <= buffer_tail + 1;
+            buffer[write_addr]  <= buf_wdata;
+            buffer_tail         <= buffer_tail + 1;
             if (buffer_tail == 10'd1023)
                 buffer_count <= buffer_count + 1;
             
@@ -124,6 +132,14 @@ always @(posedge clk) begin
         end
         STATE_READBUF_AFTER_RX: begin
             state <= STATE_IDLE;
+            if (rx_rdata_valid) begin
+                uart_rdata  <= rx_rdata;
+                state       <= STATE_READBUF_BEFORE_WRITE_MEM;
+            end
+        end
+        STATE_READBUF_BEFORE_WRITE_MEM: begin
+            state <= STATE_WRITE_MEM;
+            // ここでcmdのbuf_rdataは読めるようになってる
         end
     endcase
 end
@@ -131,10 +147,11 @@ end
 
 `ifdef DEBUG
 always @(posedge clk) begin
-    $display("uart_rx buffer[1024] %d (%d)", buffer_tail, buffer_count);
-    $display("uart_rx [%d] in: 0x%h %d %h", addr, buffer[addr], output_rdata_valid, output_rdata);
+    $display("MEMMAP UART RX-----------------");
+    $display("buffer    :%d (%d)", buffer_tail, buffer_count);
+    $display("          :%d, 0x%h %d %h", read_addr, buffer[read_addr], output_rdata_valid, output_rdata);
     if (state == STATE_IDLE && rx_rdata_valid) begin
-        $display("uart_rx : 0x%h : %d", rx_rdata, addr_mod);
+        $display("rvalid : 0x%h : %d", rx_rdata, buffer_tail[1:0]);
     end
 end
 `endif
