6a503fd017a8431f1e668ae6a4d4fb75e295e395
na <kanapodream@gmail.com>
2023-03-13 03:11:48 +0900

    Update compile.sh

--- test/c/compile.sh
+++ test/c/compile.sh
@@ -1,6 +1,11 @@
 FILE=$1
-#PATH=$PATH:/opt/riscv32/bin/
-riscv32-unknown-elf-gcc -march=rv32i -O0 -c -o temp.o $FILE
-riscv32-unknown-elf-ld temp.o -lc -L/opt/riscv32/riscv32-unknown-elf/lib/ -Tlink.ld -nostartfiles -static -o temp.elf
-riscv32-unknown-elf-objcopy -O binary temp.elf temp.bin
-echo "temp.bin" | python ../bin2hex.py
+BINFILE="${FILE}.bin"
+OBJFILE="temp.obj"
+ELFFILE="temp.elf"
+DUMPFILE="${BINFILE}.dump"
+riscv32-unknown-elf-gcc -march=rv32i -O0 -c -o $OBJFILE $FILE
+riscv32-unknown-elf-ld $OBJFILE -lc -L/opt/riscv32/riscv32-unknown-elf/lib/ -Tlink.ld -nostartfiles -static -o $ELFFILE
+riscv32-unknown-elf-objcopy -O binary $ELFFILE $BINFILE
+echo $BINFILE | python3 ../bin2hex.py
+riscv32-unknown-elf-objdump -d $ELFFILE > $DUMPFILE
+rm $BINFILE $OBJFILE $ELFFILE
