54152d1c84ce02907e490638d201d950184839eb
na <kanapodream@gmail.com>
2023-03-12 10:14:28 +0900

    UART RXの分岐を作成

--- src/MemoryMapController.v
+++ src/MemoryMapController.v
@@ -1,8 +1,3 @@
-/*
-ff000000 - ff0000ff : 送信したい文字列置き場
-ff000100 : キューの末尾のindex(送信するときはこれを進める)
-ff000104 : キューの先頭のindex(読み込みのみ)
-*/
 module MemoryMapController #(
     parameter MEMORY_SIZE = 4096,
     parameter MEMORY_FILE = ""
@@ -24,17 +19,23 @@ module MemoryMapController #(
 
 `include "include/memoryinterface.v"
 
-localparam UART_TX_BUFFER_OFFSET       = 32'hff000000;
-localparam UART_TX_QUEUE_TAIL_OFFSET   = 32'hff000100;
-localparam UART_TX_QUEUE_HEAD_OFFSET   = 32'hff000104;
+localparam UART_TX_QUEUE_OFFSET         = 32'hff000000;
+localparam UART_TX_QUEUE_TAIL_OFFSET    = 32'hff000100;
+localparam UART_TX_QUEUE_HEAD_OFFSET    = 32'hff000104;
 
-wire is_uart_tx_addr    = UART_TX_BUFFER_OFFSET <= input_addr && input_addr <= 32'hff000104;
+localparam UART_RX_BUFFER_OFFSET        = 32'hff000200;
+localparam UART_RX_BUFFER_TAIL_OFFSET   = 32'hff000600;
+localparam UART_RX_BUFFER_COUNT_OFFSET  = 32'hff000601;
+
+wire is_uart_tx_addr    = UART_TX_QUEUE_OFFSET <= input_addr && input_addr <= UART_TX_QUEUE_HEAD_OFFSET;
+wire is_uart_rx_addr    = UART_RX_BUFFER_OFFSET <= input_addr && input_addr <= UART_RX_BUFFER_COUNT_OFFSET;
+wire is_default_memory  = !is_uart_tx_addr && !is_uart_rx_addr;
 
 // UART_TX
 wire        uart_tx_cmd_start   = is_uart_tx_addr ? input_cmd_start : 0;
 wire        uart_tx_cmd_write   = is_uart_tx_addr ? input_cmd_write : 0;
 wire        uart_tx_cmd_ready;
-wire [31:0] uart_tx_addr        = input_addr - UART_TX_BUFFER_OFFSET;
+wire [31:0] uart_tx_addr        = input_addr - UART_TX_QUEUE_OFFSET;
 wire [31:0] uart_tx_rdata;
 wire        uart_tx_rdata_valid;
 wire [31:0] uart_tx_wdata       = input_wdata;
@@ -52,7 +53,27 @@ MemoryMappedIO_Uart_tx #() memmap_uarttx (
     .input_wdata(uart_tx_wdata)
 );
 
-wire is_default_memory  = !is_uart_tx_addr;
+// UART_RX
+wire        uart_rx_cmd_start   = is_uart_rx_addr ? input_cmd_start : 0;
+wire        uart_rx_cmd_write   = is_uart_rx_addr ? input_cmd_write : 0;
+wire        uart_rx_cmd_ready;
+wire [31:0] uart_rx_addr        = input_addr - UART_RX_BUFFER_OFFSET;
+wire [31:0] uart_rx_rdata;
+wire        uart_rx_rdata_valid;
+wire [31:0] uart_rx_wdata       = input_wdata;
+
+MemoryMappedIO_Uart_rx #() memmap_uarttx (
+    .clk(clk),
+    .uart_rx(uart_rx),
+
+    .input_cmd_start(uart_rx_cmd_start),
+    .input_cmd_write(uart_rx_cmd_write),
+    .output_cmd_ready(uart_rx_cmd_ready),
+    .input_addr(uart_rx_addr),
+    .output_rdata(uart_rx_rdata),
+    .output_rdata_valid(uart_rx_rdata_valid),
+    .input_wdata(uart_rx_wdata)
+);
 
 // メモリ
 wire        mem_cmd_start   = is_default_memory ? input_cmd_start : 0;
@@ -92,8 +113,14 @@ always @(posedge clk) begin
 end
 */
 
-assign output_cmd_ready     = is_uart_tx_addr ? uart_tx_cmd_ready : mem_cmd_ready;
-assign output_rdata         = is_uart_tx_addr ? uart_tx_rdata : mem_rdata;
-assign output_rdata_valid   = is_uart_tx_addr ? uart_tx_rdata_valid : mem_rdata_valid;
+assign output_cmd_ready     =   is_uart_tx_addr ? uart_tx_cmd_ready : 
+                                is_uart_rx_addr ? uart_rx_cmd_ready : 
+                                mem_cmd_ready;
+assign output_rdata         =   is_uart_tx_addr ? uart_tx_rdata : 
+                                is_uart_rx_addr ? uart_rx_rdata : 
+                                mem_rdata;
+assign output_rdata_valid   =   is_uart_tx_addr ? uart_tx_rdata_valid : 
+                                is_uart_rx_addr ? uart_rx_rdata_valid : 
+                                mem_rdata_valid;
 
 endmodule
