ceb49fec11f8afc6cc685732eedb8700e7ac8702
na <kanapodream@gmail.com>
2023-03-13 01:21:35 +0900

    バッファがメモリとして推論されるようにした

--- src/MemoryMappedIO_Uart_rx.v
+++ src/MemoryMappedIO_Uart_rx.v
@@ -15,27 +15,24 @@ module MemoryMappedIO_Uart_rx
 
 `include "include/memorymap.v"
 
-assign output_cmd_ready     = 1;
-assign output_rdata_valid   = 1;
+localparam STATE_IDLE               = 0;
+localparam STATE_WRITE_MEM          = 1;
+localparam STATE_READBUF_AFTER_RX   = 2;
 
+// メモリ
 reg [31:0] buffer[255:0];
 reg [9:0]  buffer_tail  = 0;
 reg [31:0] buffer_count = 0;
-wire [7:0] buffer_addr  = input_addr[9:2];
+
+`ifdef DEBUG
+initial begin
+    $readmemh("../test/bin/mem256zero.hex", buffer);
+end
+`endif
 
 wire is_tail_addr   = input_addr == UART_RX_BUFFER_TAIL_OFFSET;
 wire is_count_addr  = input_addr == UART_RX_BUFFER_COUNT_OFFSET;
 wire is_buffer_addr = !is_count_addr && !is_tail_addr; 
-
-// メモリ (書き込み不可)
-always @(posedge clk) begin
-    if (is_buffer_addr)
-        output_rdata <= buffer[buffer_addr];
-    else if (is_tail_addr)
-        output_rdata <= {22'b0, buffer_tail};
-    else if (is_count_addr)
-        output_rdata <= buffer_count;     
-end
 
 // UART
 wire [7:0]  rx_rdata;
@@ -49,30 +46,30 @@ Uart_rx #() rxModule(
 );
 
 // アドレスは4byteずつ
-wire [7:0] addr     = buffer_tail[9:2]; // buffer_tail >> 2
+function [7:0] func_addr(
+    input [1:0] state,
+    input       rx_rdata_valid,
+    input [9:0] buffer_tail,
+    input [31:0]input_addr
+);
+    case (state)
+        STATE_IDLE              : func_addr = rx_rdata_valid ? buffer_tail[9:2] : input_addr[9:2];
+        STATE_WRITE_MEM         : func_addr = buffer_tail[9:2];
+        STATE_READBUF_AFTER_RX  : func_addr = input_addr[9:2];
+        default                 : func_addr = 0;
+    endcase
+endfunction
+
+wire [7:0] addr     = func_addr(state, rx_rdata_valid, buffer_tail, input_addr);
 wire [1:0] addr_mod = buffer_tail[1:0]; // buffer_tail % 4
 
 // 状態
-localparam STATE_IDLE       = 0;
-localparam STATE_WRITE_MEM  = 1;
-
-reg state = STATE_IDLE;
+reg [1:0] state = STATE_IDLE;
 
 // バッファから読んだデータ
 reg [31:0] buf_rdata = 0;
 // UART
 reg [7:0] uart_rdata = 0;
-
-/*
-`ifdef DEBUG
-always @(posedge clk) begin
-    $display("uart_rx buffer[1024] %d (%d)", buffer_tail, buffer_count);
-    if (rx_rdata_valid) begin
-        $display("uart_rx : 0x%h : %d", rdata, addr_mod);
-    end
-end
-`endif
-*/
 
 function [31:0] func_buf_wdata(
     input [31:0] buf_rdata,
@@ -88,20 +85,59 @@ function [31:0] func_buf_wdata(
 endfunction
 wire [31:0] buf_wdata = func_buf_wdata(buf_rdata, uart_rdata, addr_mod);
 
+assign output_cmd_ready     = state == STATE_IDLE;
+assign output_rdata_valid   = state == STATE_IDLE;
+
+reg save_input_cmd_start = 0;
+reg save_input_cmd_write = 0;
+
 always @(posedge clk) begin
+
+    if (is_tail_addr)
+        output_rdata <= {22'b0, buffer_tail};
+    else if (is_count_addr)
+        output_rdata <= buffer_count;
+    else if (is_buffer_addr)
+        output_rdata <= buffer[addr];
+
+    buf_rdata            <= buffer[addr];
+    save_input_cmd_start <= input_cmd_start;
+    save_input_cmd_write <= input_cmd_write;
+
     case (state)
         STATE_IDLE: begin
             if (rx_rdata_valid) begin
                 state       <= STATE_WRITE_MEM;
-                buf_rdata   <= buffer[addr];
                 uart_rdata  <= rx_rdata;
             end
         end
         STATE_WRITE_MEM: begin
-            buffer[addr] <= buf_wdata;
-            buffer_tail += 1;
+            buffer[addr]    <= buf_wdata;
+            buffer_tail     <= buffer_tail + 1;
+            if (buffer_tail == 10'd1023)
+                buffer_count <= buffer_count + 1;
+            
+            if (save_input_cmd_start && is_buffer_addr)
+                state <= STATE_READBUF_AFTER_RX;
+            else
+                state <= STATE_IDLE;
+        end
+        STATE_READBUF_AFTER_RX: begin
+            state <= STATE_IDLE;
         end
     endcase
 end
+
+
+`ifdef DEBUG
+always @(posedge clk) begin
+    $display("uart_rx buffer[1024] %d (%d)", buffer_tail, buffer_count);
+    $display("uart_rx [%d] in: 0x%h %d %h", addr, buffer[addr], output_rdata_valid, output_rdata);
+    if (state == STATE_IDLE && rx_rdata_valid) begin
+        $display("uart_rx : 0x%h : %d", rx_rdata, addr_mod);
+    end
+end
+`endif
+
 
 endmodule
