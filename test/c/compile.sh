d8415f8ab9ffd8837931ec7b1a8f444f68b77aae
na <kanapodream@gmail.com>
2023-03-17 07:21:01 +0900

    ターゲットをrv32imにした

--- test/c/compile.sh
+++ test/c/compile.sh
@@ -3,7 +3,9 @@ BINFILE="${FILE}.bin"
 OBJFILE="temp.obj"
 ELFFILE="temp.elf"
 DUMPFILE="${BINFILE}.dump"
-riscv32-unknown-elf-gcc -march=rv32i -O0 -c -o $OBJFILE $FILE
+echo $FILE
+echo $OBJFILE
+riscv32-unknown-elf-gcc -march=rv32im -O0 -c -o $OBJFILE $FILE
 riscv32-unknown-elf-ld $OBJFILE -lc -L/opt/riscv32/riscv32-unknown-elf/lib/ -Tlink.ld -nostartfiles -static -o $ELFFILE
 riscv32-unknown-elf-objcopy -O binary $ELFFILE $BINFILE
 echo $BINFILE | python3 ../bin2hex.py
