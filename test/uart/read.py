6e043f2e6076a507e70e550dfaebd82a9bc0eac5
na <kanapodream@gmail.com>
2023-03-17 06:04:06 +0900

    リトルエンディアンでも値を表示する

--- test/uart/read.py
+++ test/uart/read.py
@@ -6,9 +6,9 @@ if len(p) == 0:
 ser = serial.Serial(p, 115200)
 
 def read1ByteInt():
-    return ord(ser.read())
+    return ord(ser.read(1))
 
 while True:
     i = read1ByteInt()
     c = chr(i)
-    print(i, ":", c, bin(i))
+    print(i, ":", c, bin(i), "le:", "".join(list(reversed(bin(i)[2:]))))
