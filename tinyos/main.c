bbb441a303de95ac8220a9645daab5fad4afebf6
na <kanapodream@gmail.com>
2023-03-21 09:28:14 +0900

    mainから各プログラムを初期化したあと、panicを起こすようにした

--- tinyos/main.c
+++ tinyos/main.c
@@ -1,4 +1,14 @@
+#include "riscv.h"
+
+void consoleinit();
+void uartinit();
+void printfinit();
+
 void main(void)
 {
-    
+    uartinit();
+    consoleinit();
+    printfinit();
+
+    panic("hello");
 }
