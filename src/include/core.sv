a220cfd5b356b5e846844c0a6d31b45a2ce48e96
na <kanapodream@gmail.com>
2023-05-26 15:13:08 +0900

    ctrltypeのtypedefをcore.svからctrltype.svに移動

--- src/include/core.sv
+++ src/include/core.sv
@@ -81,23 +81,3 @@ localparam CSR_C    = 3'd3;
 localparam CSR_ECALL= 3'd4;
 localparam CSR_SRET = 3'd5;
 localparam CSR_MRET = 3'd6;
-
-typedef struct packed 
-{
-    reg [4:0]   exe_fun;
-    reg [31:0]  op1_data;
-    reg [31:0]  op2_data;
-    reg [31:0]  rs2_data;
-    reg [3:0]   mem_wen;
-    reg         rf_wen;
-    reg [3:0]   wb_sel;
-    reg [4:0]   wb_addr;
-    reg [2:0]   csr_cmd;
-    reg         jmp_flg;
-    reg [31:0]  imm_i_sext;
-    reg [31:0]  imm_s_sext;
-    reg [31:0]  imm_b_sext;
-    reg [31:0]  imm_j_sext;
-    reg [31:0]  imm_u_shifted;
-    reg [31:0]  imm_z_uext;
-} ctrltype;
