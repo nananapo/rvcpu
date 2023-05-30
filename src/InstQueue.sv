19e476a96343c223290aa22dc13aed1aaf0ddb5f
kanataso <kanapo.go@gmail.com>
2023-05-30 19:55:50 +0900

    InstQueueを実装
    全く動作確認してない

--- src/InstQueue.sv
+++ src/InstQueue.sv
@@ -17,31 +17,56 @@ reg [31:0]  inst_queue[QUEUE_SIZE-1:0];
 reg [31:0]  pc          = 32'd0;
 reg [63:0]  inst_id     = 64'd0;
 
+reg         requested   = 0;
 reg [31:0]  request_pc  = 32'd0;
 
-assign memreq.valid    = ireq.valid || // 分岐ハザードなら強制的にvalid
-                            (queue_tail + 1) % QUEUE_SIZE == queue_head; // キューがフルではないならとりあえずvalid
-assign memreq.addr     = ireq.valid ? ireq.addr : pc;
+wire [31:0] next_pc     = pc;
+wire queue_is_full      = ireq.valid || //分岐ハザードなら空
+                          (queue_tail + 1) % QUEUE_SIZE == queue_head; // キューがフル
 
-assign iresp.valid      = queue_head != queue_tail;
+assign memreq.valid     = !queue_is_full;
+assign memreq.addr      = ireq.valid ? ireq.addr : pc;
+
+assign iresp.valid      = queue_head != queue_tail && !ireq.valid;
 assign iresp.addr       = pc_queue[queue_head];
 assign iresp.inst       = inst_queue[queue_head];
 assign iresp.inst_id    = inst_id;
 
 always @(posedge clk) begin
+    // ireq.valid -> 分岐予測に失敗
     if (ireq.valid) begin
-        // ireq.valid -> 分岐予測に失敗
-        // キューのサイズを0にして、pcにireq.addrを設定する。
+        // pcにireq.addrを設定する。
         pc          <= ireq.addr;
-        queue_head  <= queue_tail;
         inst_id     <= inst_id + 1;
-    end else begin
-        // リクエストが完了した
-        if (memresp.valid && memresp.addr == request_pc) begin
-            // キューが
-            if (queue_head == queue_tail) begin
+        queue_head  <= queue_tail; // キューのサイズを0にする
 
+        // リクエスト状態を設定
+        requested   <= memreq.ready;
+	request_pc  <= ireq.addr;
+
+        // TODO $display(予測失敗)
+    end else begin
+        if (requested) begin
+            // リクエストが完了した
+            if (memresp.valid && memresp.addr == request_pc) begin
+                pc                      <= next_pc;
+                queue_tail              <= (queue_tail + 1) % QUEUE_SIZE;
+                pc_queue[queue_tail]    <= memresp.addr;
+                inst_queue[queue_tail]  <= memresp.inst;
+                // TODO $display(inst_id)
             end
+        end else begin
+            // メモリがreadyならリクエスト
+            if (memreq.ready) begin
+                pc         <= next_pc;
+                requested  <= 1;
+                request_pc <= pc;
+            end
+        end
+        // if/idがキューから1つ命令を取得する
+        if (iresp.ready) begin
+            queue_head  <= queue_head + 1;
+            inst_id     <= inst_id + 1;
         end
     end
 end
