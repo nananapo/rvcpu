eccf7d11ee8ab555070c7dc1236f651952d5d6cb
na <kanapodream@gmail.com>
2023-06-11 16:01:41 +0900

    coremarkが結果を出力したら終了するようにした

---
+++ .github/workflows/coremark.py
@@ -0,0 +1,22 @@
+import sys
+import subprocess
+
+def get_lines(cmd):
+    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+    while True:
+        line = proc.stdout.readline()
+        if line:
+            yield line
+        if not line and proc.poll() is not None:
+            break
+
+startCount = 0
+for line in get_lines(cmd='cd ../../src && timeout 7200 obj_dir/Vtest_verilator'):
+    print(line)
+    if "Correct operation validated" in line:
+        exit(0)
+    if "start" in line:
+        startCount += 1
+        if startCount == 2:
+            exit(1)
+exit(1)
