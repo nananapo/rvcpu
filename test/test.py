a88c455c17f7ef90c82397f87c393bb4855fcab5
kanataso <kanapo.go@gmail.com>
2023-08-31 04:26:26 +0900

    riscv-testsが動くようにする

--- test/test.py
+++ test/test.py
@@ -1,14 +1,8 @@
 import sys
 
-MEMORY_V_FILENAME = "../src/main.sv"
-REPLACE_WORD = "MEMORY_FILE_NAME"
+TESTS_PATH = "riscv-tests/"
 MAKE_COMMAND_IVERILOG = "make riscv-tests"
-MAKE_COMMAND_VERILATOR = "make dvrv"
-
-# backup
-memory_backup = ""
-with open(MEMORY_V_FILENAME, "r", encoding='utf-8') as f:
-    memory_backup = "".join(f.readlines())
+MAKE_COMMAND_VERILATOR = "make dvrv "
 
 from os import system
 import os
@@ -18,13 +12,11 @@ os.makedirs("../test/results", exist_ok=True)
 results = []
 resultstatus = []
 def test(makecmd, filename):
-    # replace
-    memory_v_test = memory_backup.replace(REPLACE_WORD, filename)
-    with open(MEMORY_V_FILENAME, "w", encoding='utf-8') as f:
-        f.write(memory_v_test)
     # run test
     resultFileName = "../test/results/" + filename.replace("/","_") + ".txt"
-    system("cd ../src/ && " + makecmd + " > " + resultFileName)
+    cmd = "cd ../src/ && " + makecmd + " > " + resultFileName
+    # print(cmd)
+    system(cmd)
 
     with open(resultFileName, "r") as f:
         result = "".join(f.readlines())
@@ -43,11 +35,15 @@ if len(args) >= 1 and args[0] == "-v":
     verilator_mode = True
     args = args[1:]
 
-for fileName in sorted(os.listdir("riscv-tests/")):
+for fileName in sorted(os.listdir(TESTS_PATH)):
     if not fileName.endswith(".aligned"):
         continue
+    abpath = os.getcwd() + "/" + TESTS_PATH + "/" + fileName
+
     if len(args) == 0 or fileName.find(args[0]) != -1:
-        test(MAKE_COMMAND_VERILATOR if verilator_mode else MAKE_COMMAND_IVERILOG, fileName)
+        mcmd = MAKE_COMMAND_VERILATOR if verilator_mode else MAKE_COMMAND_IVERILOG
+        option = " OPTION=-DMEM_FILE=\\\\\\\""+abpath+"\\\\\\\""
+        test(mcmd + option, fileName)
 
 results = sorted(results)
 
@@ -57,10 +53,6 @@ statusText = "Test Result : " + successCount + " / " + str(len(resultstatus))
 with open("results/result.txt", "w", encoding='utf-8') as f:
     f.write(statusText + "\n")
     f.write("\n".join(results))
-
-# rollback
-with open(MEMORY_V_FILENAME, "w", encoding='utf-8') as f:
-    f.write(memory_backup)
 
 print(statusText)
 
