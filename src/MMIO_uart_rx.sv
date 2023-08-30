238421471da0a419c81e8bf455536c23122f6efa
kanataso <kanapo.go@gmail.com>
2023-08-31 05:19:10 +0900

    uart rxのmmioの仕様変更

--- src/MMIO_uart_rx.sv
+++ src/MMIO_uart_rx.sv
@@ -1,5 +1,6 @@
 module MMIO_uart_rx #(
-    parameter FMAX_MHz = 27
+    parameter FMAX_MHz  = 27,
+    parameter BUF_WIDTH = 10
 )(
     input  wire clk,
     input  wire uart_rx,
@@ -14,30 +15,15 @@ module MMIO_uart_rx #(
     output UInt32   resp_rdata
 );
 
-`include "include/memorymap.sv"
+localparam BUF_LEN = 2**BUF_WIDTH;
+typedef logic [BUF_WIDTH-1:0] BufWidth;
 
-localparam STATE_IDLE                       = 0;
-localparam STATE_WRITE_MEM                  = 1;
-localparam STATE_READBUF_AFTER_RX           = 2;
-localparam STATE_READBUF_BEFORE_WRITE_MEM   = 3;
-
-// メモリ
-logic [31:0] buffer[255:0];
-logic [9:0]  buffer_tail  = 0;
-logic [31:0] buffer_count = 0;
-
-`ifdef DEBUG
-initial begin
-    $readmemh("../test/bin/mem256zero.hex", buffer, 0, 255);
-end
-`endif
-
-wire is_tail_addr   = req_addr == UART_RX_BUFFER_TAIL_OFFSET;
-wire is_count_addr  = req_addr == UART_RX_BUFFER_COUNT_OFFSET;
-wire is_buffer_addr = !is_count_addr && !is_tail_addr; 
+UInt8       buffer[BUF_LEN-1:0];
+BufWidth    head = 0;
+BufWidth    tail = 0;
 
 // UART
-wire [7:0]  rx_rdata;
+wire UInt8  rx_rdata;
 wire        rx_rvalid;
 
 Uart_rx #(
@@ -49,114 +35,22 @@ Uart_rx #(
     .uart_rx(uart_rx)
 );
 
-// アドレスは4byteずつ
-function [7:0] func_read_addr(
-    input [1:0] state,
-    input       rx_rvalid,
-    input [9:0] buffer_tail,
-    input [31:0]req_addr
-);
-    case (state)
-        STATE_IDLE                      : func_read_addr = rx_rvalid ? buffer_tail[9:2] : req_addr[9:2];
-        STATE_WRITE_MEM                 : func_read_addr = req_addr[9:2];
-        STATE_READBUF_AFTER_RX          : func_read_addr = req_addr[9:2];
-        STATE_READBUF_BEFORE_WRITE_MEM  : func_read_addr = buffer_tail[9:2];
-        default                         : func_read_addr = 0;
-    endcase
-endfunction
-
-wire [7:0] read_addr    = func_read_addr(state, rx_rvalid, buffer_tail, req_addr);
-wire [7:0] write_addr   = buffer_tail[9:2];
-
-// 状態
-logic [1:0] state = STATE_IDLE;
-
-// バッファから読んだデータ
-logic [31:0] buf_rdata = 0;
-// UART
-logic [7:0] uart_rdata = 0;
-
-function [31:0] func_buf_wdata(
-    input [31:0] buf_rdata,
-    input [7:0] uart_rdata,
-    input [1:0]  addr_mod
-);
-    case (addr_mod) 
-        0: func_buf_wdata = {buf_rdata[31:8], uart_rdata};
-        1: func_buf_wdata = {buf_rdata[31:16], uart_rdata, buf_rdata[7:0]};
-        2: func_buf_wdata = {buf_rdata[31:24], uart_rdata, buf_rdata[15:0]};
-        3: func_buf_wdata = {uart_rdata, buf_rdata[23:0]};
-    endcase
-endfunction
-wire [31:0] buf_wdata = func_buf_wdata(buf_rdata, uart_rdata, buffer_tail[1:0]);
-
-assign req_ready     = state == STATE_IDLE;
-assign resp_valid   = is_tail_addr || is_count_addr || (
-    is_buffer_addr ?
-        state == STATE_READBUF_BEFORE_WRITE_MEM || state == STATE_IDLE
-        : 0
-);
-
-logic save_req_valid = 0;
-logic save_req_wen = 0;
+assign req_ready    = 1;
+assign resp_valid   = 1;
 
 always @(posedge clk) begin
-
-    if (is_tail_addr)
-        resp_rdata <= {22'b0, buffer_tail};
-    else if (is_count_addr)
-        resp_rdata <= buffer_count;
-    else if (is_buffer_addr)
-        resp_rdata <= buf_rdata;
-
-    buf_rdata            <= buffer[read_addr];
-    save_req_valid <= req_valid;
-    save_req_wen <= req_wen;
-
-    case (state)
-        STATE_IDLE: begin
-            if (rx_rvalid) begin
-                state       <= STATE_WRITE_MEM;
-                uart_rdata  <= rx_rdata;
-            end else if (req_valid) begin
-                state       <= STATE_READBUF_AFTER_RX;
-            end
+    if (rx_rvalid) begin
+        buffer[tail] <= rx_rdata;
+        tail <= tail + 1;
+    end
+    if (req_valid) begin
+        if (head != tail) begin
+            resp_rdata <= buffer[head];
+            head <= head + 1;
+        end else begin
+            resp_rdata <= 0;
         end
-        STATE_WRITE_MEM: begin
-            buffer[write_addr]  <= buf_wdata;
-            buffer_tail         <= buffer_tail + 1;
-            if (buffer_tail == 10'd1023)
-                buffer_count <= buffer_count + 1;
-            
-            if (save_req_valid && is_buffer_addr)
-                state <= STATE_READBUF_AFTER_RX;
-            else
-                state <= STATE_IDLE;
-        end
-        STATE_READBUF_AFTER_RX: begin
-            state <= STATE_IDLE;
-            if (rx_rvalid) begin
-                uart_rdata  <= rx_rdata;
-                state       <= STATE_READBUF_BEFORE_WRITE_MEM;
-            end
-        end
-        STATE_READBUF_BEFORE_WRITE_MEM: begin
-            state <= STATE_WRITE_MEM;
-            // ここでcmdのbuf_rdataは読めるようになってる
-        end
-    endcase
+    end
 end
-
-
-`ifdef PRINT_DEBUGINFO
-always @(posedge clk) begin
-    // $display("info,memmapio.uart_rx.buffer,Buffer : %d (%d)", buffer_tail, buffer_count);
-    // $display("info,memmapio.uart_rx.buffer_data,BufferData : %d 0x%h %d %h", read_addr, buffer[read_addr], resp_valid, resp_rdata);
-    // if (state == STATE_IDLE && rx_rvalid) begin
-    //      $display("info,memmapio.uart_rx.rvalid, rvalid 0x%h : %d", rx_rdata, buffer_tail[1:0]);
-    // end
-end
-`endif
-
 
 endmodule
--- src/include/memorymap.sv
+++ src/include/memorymap.sv
@@ -1,10 +1,5 @@
 localparam MMIO_ADDR_UART_TX = 32'hff000000;
-
-localparam UART_RX_OFFSET               = 32'hff000200;
-localparam UART_RX_BUFFER_OFFSET        = 32'h0;
-localparam UART_RX_BUFFER_TAIL_OFFSET   = 32'h400;
-localparam UART_RX_BUFFER_COUNT_OFFSET  = 32'h404;
-localparam UART_RX_END                  = UART_RX_OFFSET + UART_RX_BUFFER_COUNT_OFFSET + 4;
+localparam MMIO_ADDR_UART_RX = 32'hff000200;
 
 localparam CLINT_OFFSET        = 32'hf0000000;
 localparam CLINT_MTIME         = 32'h0;
