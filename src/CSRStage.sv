64671353d01c97c326802231723046ed9d5b4600
kanataso <kanapo.go@gmail.com>
2023-10-20 00:22:47 +0900

    CSR : ワイヤ名変更

--- src/CSRStage.sv
+++ src/CSRStage.sv
@@ -40,18 +40,21 @@ assign cache_cntr.satp  = satp;
 `include "basicparams.svh"
 
 initial begin
-    // 起動時はM-mode
+    // 起動時はM-mode  (EEI)
     mode = M_MODE;
-    // 5.1.11
-    // MODE(1) | ASID(9) | PPN(22)
-    // Table 23
-    // MODE = 0 : Bare (物理アドレスと同じ), ASID, PPNも0にする必要がある
-    //            0ではないなら動作はUNSPECIFIED！こわいね
-    // MODE = 1 : Sv32, ページングが有効
     satp = 0;
 end
 
 wire CsrCmd csr_cmd = ctrl.csr_cmd;
+wire UInt12 addr    = imm_i[11:0];
+
+// 2.1 CSR Address Mapping Conventions
+wire can_access     = addr[9:8] <= mode;
+wire can_read       = can_access;
+wire can_write      = can_access & addr[11:10] != 2'b11;
+
+wire cmd_is_write   = csr_cmd == CSR_W | csr_cmd == CSR_S | csr_cmd == CSR_C;
+wire cmd_is_xret    = csr_cmd == CSR_SRET | csr_cmd == CSR_MRET;
 
 typedef enum logic [11:0] {
     // Counters and Timers
@@ -126,6 +129,83 @@ typedef enum logic [1:0] {
     XTVEC_DIRECT   = 2'b00,
     XTVEC_VECTORED = 2'b01
 } xtvec_mode_type;
+
+function [31:0] gen_wdata(
+    input [2:0]  csr_cmd,
+    input [31:0] op1_data,
+    input [31:0] rdata
+);
+case (csr_cmd)
+    CSR_W  : gen_wdata = op1_data;
+    CSR_S  : gen_wdata = rdata | op1_data;
+    CSR_C  : gen_wdata = rdata & ~op1_data;
+    default: gen_wdata = 0;
+endcase
+endfunction
+
+function [31:0] gen_rdata(
+    input UInt12 addr,
+    input [63:0] reg_cycle,
+    input [63:0] reg_time,
+    input [31:0] mstatus,
+    input [31:0] sstatus,
+    input [31:0] mstatush,
+    input [31:0] misa,
+    input [31:0] medeleg,
+    input [31:0] mideleg,
+    input [31:0] mie,
+    input [31:0] sie,
+    input [31:0] mscratch,
+    input [31:0] mepc,
+    input [31:0] mcause,
+    input [31:0] mip,
+    input [31:0] sip,
+    input [31:0] mtinst,
+    input [31:0] mtvec2,
+    input [31:0] sscratch,
+    input [31:0] sepc,
+    input [31:0] scause,
+    input [31:0] satp
+);
+case (addr)
+    // Counters and Timers
+    ADDR_CYCLE:     gen_rdata = reg_cycle[31:0];
+    ADDR_TIME:      gen_rdata = reg_time[31:0];
+    ADDR_CYCLEH:    gen_rdata = reg_cycle[63:32];
+    ADDR_TIMEH:     gen_rdata = reg_time[63:32];
+    // Machine Trap Setup
+    ADDR_MSTATUS:   gen_rdata = mstatus;
+    ADDR_MISA:      gen_rdata = misa;
+    ADDR_MEDELEG:   gen_rdata = medeleg;
+    ADDR_MIDELEG:   gen_rdata = mideleg;
+    ADDR_MIE:       gen_rdata = mie;
+    ADDR_MTVEC:     gen_rdata = mtvec;
+    ADDR_MSTATUSH:  gen_rdata = mstatush;
+    // Machine Trap Handling
+    ADDR_MSCRATCH:  gen_rdata = mscratch;
+    ADDR_MEPC:      gen_rdata = mepc;
+    ADDR_MCAUSE:    gen_rdata = mcause;
+    ADDR_MIP:       gen_rdata = mip;
+    ADDR_MTINST:    gen_rdata = mtinst;
+    ADDR_MTVAL2:    gen_rdata = mtvec2;
+    // Machine Counter/Timers
+    ADDR_MCYCLE:    gen_rdata = reg_cycle[31:0];
+    ADDR_MCYCLEH:   gen_rdata = reg_cycle[63:32];
+    // Supervisor Trap Setup
+    ADDR_SSTATUS:   gen_rdata = sstatus;
+    ADDR_SIE:       gen_rdata = sie;
+    ADDR_STVEC:     gen_rdata = stvec;
+    // Supervisor Trap Handling
+    ADDR_SSCRATCH:  gen_rdata = sscratch;
+    ADDR_SEPC:      gen_rdata = sepc;
+    ADDR_SCAUSE:    gen_rdata = scause;
+    // ADDR_STVAL:     gen_rdata = stval;
+    ADDR_SIP:       gen_rdata = sip;
+    // Supervisor Protection and Translation
+    ADDR_SATP:      gen_rdata = satp;
+    default:        gen_rdata = 32'b0;
+endcase
+endfunction
 
 wire mstatus_sd  = 0;
 
@@ -215,15 +295,15 @@ logic [31:0] medeleg = 0;
 // 0 SGEIP MEIP VSEIP SEIP 0 MTIP VSTIP STIP 0 MSIP VSSIP SSIP 0
 logic [15:0] mideleg_custom = 0;
 wire mideleg_sgeip  = 0; // any guest external interruptsをサポートする
-logic mideleg_meip    = 0;
+logic mideleg_meip  = 0;
 wire mideleg_vseip  = 0; // hypervisor extensionをサポートしない
-logic mideleg_seip    = 0;
-logic mideleg_mtip    = 0;
+logic mideleg_seip  = 0;
+logic mideleg_mtip  = 0;
 wire mideleg_vstip  = 0; // hypervisor extensionをサポートしない
-logic mideleg_stip    = 0;
-logic mideleg_msip    = 0;
+logic mideleg_stip  = 0;
+logic mideleg_msip  = 0;
 wire mideleg_vssip  = 0; // hypervisor extensionをサポートしない
-logic mideleg_ssip    = 0;
+logic mideleg_ssip  = 0;
 wire [31:0] mideleg = {
     mideleg_custom,
     3'b0,
@@ -314,7 +394,25 @@ wire [31:0] stvec_addr = stvec[1:0] == XTVEC_DIRECT ? stvec : {stvec[31:2], 2'b0
 wire global_mie = mode == M_MODE ? mstatus_mie : 1;
 wire global_sie = mode == S_MODE ? mstatus_sie : mode == U_MODE;
 
-// wire gg
+// TODO interruptが発生しないのに発生してしまう?かと思ったが、0が0なので発生しないようだ
+
+// 3.1.8. Machine Trap Delegation Registers (medeleg and mideleg)
+// S-modeに委譲されているとき、M-modeならM-modeにトラップする。S-mode, U-modeならS-modeにトラップする。
+wire interrupt_to_mmode = mideleg[interrupt_cause[4:0]] == 0;
+wire exception_to_mmode = medeleg[exception_cause[4:0]] == 0;
+wire trap_nochange = ctrl.fence_i;
+wire trap_to_mmode = mode == M_MODE | (trapinfo.valid ? exception_to_mmode : interrupt_to_mmode);
+
+// trapが起こりそうかどうか
+wire raise_interrupt = (interrupt_to_mmode ? global_mie : global_sie) &
+(
+    (mip_meip & mie_meie) |
+    (mip_msip & mie_msie) |
+    (mip_mtip & mie_mtie) |
+    (mip_seip & mie_seie) |
+    (mip_ssip & mie_ssie) |
+    (mip_stip & mie_stie)
+);
 
 // 3.1.9
 // Multiple simultaneous interrupts destined for M-mode are handled in the following decreasing
@@ -332,106 +430,8 @@ wire [31:0] interrupt_cause = (
 wire [31:0] exception_cause = trapinfo.cause + (csr_cmd == CSR_ECALL ? {30'b0, mode} : 0);
 wire [31:0] trap_cause = trapinfo.valid ? exception_cause : interrupt_cause;
 
-// 3.1.8. Machine Trap Delegation Registers (medeleg and mideleg)
-// S-modeに委譲されているとき、M-modeならM-modeにトラップする。S-mode, U-modeならS-modeにトラップする。
-wire interrupt_to_mmode = mideleg[interrupt_cause[4:0]] == 0;
-wire exception_to_mmode = medeleg[exception_cause[4:0]] == 0;
-wire trap_nochange = ctrl.fence_i;
-wire trap_to_mmode = mode == M_MODE | (trapinfo.valid ? exception_to_mmode : interrupt_to_mmode);
-
-// trapが起こりそうかどうか
-wire may_interrupt = (interrupt_to_mmode ? global_mie : global_sie) &
-(
-    (mip_meip & mie_meie) |
-    (mip_msip & mie_msie) |
-    (mip_mtip & mie_mtie) |
-    (mip_seip & mie_seie) |
-    (mip_ssip & mie_ssie) |
-    (mip_stip & mie_stie)
-);
-
-wire UInt12 addr = imm_i[11:0];
-
-function [31:0] wdata_fun(
-    input [2:0]  csr_cmd,
-    input [31:0] op1_data,
-    input [31:0] rdata
-);
-case (csr_cmd)
-    CSR_W  : wdata_fun = op1_data;
-    CSR_S  : wdata_fun = rdata | op1_data;
-    CSR_C  : wdata_fun = rdata & ~op1_data;
-    default: wdata_fun = 0;
-endcase
-endfunction
-
-function [31:0] gen_rdata(
-    input UInt12 addr,
-    input [63:0] reg_cycle,
-    input [63:0] reg_time,
-    input [31:0] mstatus,
-    input [31:0] sstatus,
-    input [31:0] mstatush,
-    input [31:0] misa,
-    input [31:0] medeleg,
-    input [31:0] mideleg,
-    input [31:0] mie,
-    input [31:0] sie,
-    input [31:0] mscratch,
-    input [31:0] mepc,
-    input [31:0] mcause,
-    input [31:0] mip,
-    input [31:0] sip,
-    input [31:0] mtinst,
-    input [31:0] mtvec2,
-    input [31:0] sscratch,
-    input [31:0] sepc,
-    input [31:0] scause,
-    input [31:0] satp
-);
-case (addr)
-    // Counters and Timers
-    ADDR_CYCLE:     gen_rdata = reg_cycle[31:0];
-    ADDR_TIME:      gen_rdata = reg_time[31:0];
-    ADDR_CYCLEH:    gen_rdata = reg_cycle[63:32];
-    ADDR_TIMEH:     gen_rdata = reg_time[63:32];
-    // Machine Trap Setup
-    ADDR_MSTATUS:   gen_rdata = mstatus;
-    ADDR_MISA:      gen_rdata = misa;
-    ADDR_MEDELEG:   gen_rdata = medeleg;
-    ADDR_MIDELEG:   gen_rdata = mideleg;
-    ADDR_MIE:       gen_rdata = mie;
-    ADDR_MTVEC:     gen_rdata = mtvec;
-    ADDR_MSTATUSH:  gen_rdata = mstatush;
-    // Machine Trap Handling
-    ADDR_MSCRATCH:  gen_rdata = mscratch;
-    ADDR_MEPC:      gen_rdata = mepc;
-    ADDR_MCAUSE:    gen_rdata = mcause;
-    ADDR_MIP:       gen_rdata = mip;
-    ADDR_MTINST:    gen_rdata = mtinst;
-    ADDR_MTVAL2:    gen_rdata = mtvec2;
-    // Machine Counter/Timers
-    ADDR_MCYCLE:    gen_rdata = reg_cycle[31:0];
-    ADDR_MCYCLEH:   gen_rdata = reg_cycle[63:32];
-    // Supervisor Trap Setup
-    ADDR_SSTATUS:   gen_rdata = sstatus;
-    ADDR_SIE:       gen_rdata = sie;
-    ADDR_STVEC:     gen_rdata = stvec;
-    // Supervisor Trap Handling
-    ADDR_SSCRATCH:  gen_rdata = sscratch;
-    ADDR_SEPC:      gen_rdata = sepc;
-    ADDR_SCAUSE:    gen_rdata = scause;
-    // ADDR_STVAL:     gen_rdata = stval;
-    ADDR_SIP:       gen_rdata = sip;
-    // Supervisor Protection and Translation
-    ADDR_SATP:      gen_rdata = satp;
-    default:        gen_rdata = 32'b0;
-endcase
-endfunction
-
-wire UIntX  wdata = wdata_fun(csr_cmd, op1_data, rdata);
-
-wire UIntX  rdata = can_read ? gen_rdata(
+wire UIntX  wdata = gen_wdata(csr_cmd, op1_data, rdata);
+wire UIntX  rdata = can_read ? gen_rdata( // TODO 例外で不要になる
     addr,
     reg_cycle,
     reg_time,
@@ -458,26 +458,18 @@ wire UIntX  rdata = can_read ? gen_rdata(
 UIntX       rdata_saved;
 assign      next_csr_rdata = rdata_saved;
 
-// 2.1 CSR Address Mapping Conventions
-wire can_access     = addr[9:8] <= mode;
-wire can_read       = can_access;
-wire can_write      = can_access & addr[11:10] != 2'b11;
-
-wire cmd_is_write   = csr_cmd == CSR_W | csr_cmd == CSR_S | csr_cmd == CSR_C;
-wire cmd_is_xret    = csr_cmd == CSR_SRET | csr_cmd == CSR_MRET;
-
-wire this_cause_trap    = trapinfo.valid | may_interrupt;
-logic last_cause_trap   = 0;
+wire this_raise_trap    = trapinfo.valid | raise_interrupt;
+logic last_raise_trap   = 0;
 
 wire undone_fence_i = ctrl.fence_i & !cache_cntr.is_writebacked_all;
 
-wire fence_clocked = ctrl.fence_i & !undone_fence_i & inst_clock == 2'b0;
+wire fence_clocked  = ctrl.fence_i & !undone_fence_i & inst_clock == 2'b0;
 
 assign is_stall     = valid & (
-                ( is_new & (this_cause_trap | cmd_is_xret | cmd_is_write | trap_nochange)) |
+                ( is_new & (this_raise_trap | cmd_is_xret | cmd_is_write | trap_nochange)) |
                 (!is_new & undone_fence_i | fence_clocked)
 );
-assign csr_is_trap  = valid & !is_new & (last_cause_trap | undone_fence_i | fence_clocked);
+assign csr_is_trap  = valid & !is_new & (last_raise_trap | undone_fence_i | fence_clocked);
 assign csr_keep_trap= trap_nochange;
 
 assign cache_cntr.do_writeback      = is_new & ctrl.fence_i;
@@ -486,7 +478,7 @@ assign cache_cntr.invalidate_icache = is_new & ctrl.fence_i;
 logic [1:0] inst_clock = 0;
 
 always @(posedge clk) begin
-    last_cause_trap <= this_cause_trap | cmd_is_xret;
+    last_raise_trap <= this_raise_trap | cmd_is_xret;
     if (valid & is_new) begin
         inst_clock <= 0;
         // trapを起こす
@@ -495,7 +487,7 @@ always @(posedge clk) begin
             `ifdef PRINT_DEBUGINFO
                 $display("info,csrstage.trap.nochange,0x%h", pc);
             `endif
-        end else if (this_cause_trap) begin
+        end else if (this_raise_trap) begin
             `ifdef PRINT_DEBUGINFO
                 $display("info,csrstage.trap.pc,0x%h", pc);
                 $display("info,csrstage.trap.to_mmode,%b", trap_to_mmode);
@@ -519,7 +511,7 @@ always @(posedge clk) begin
                 trap_vector  <= stvec_addr;
             end
             // interruptならmipを0にする
-            if (!trapinfo.valid & may_interrupt) begin
+            if (!trapinfo.valid & raise_interrupt) begin
                      if (mip_meip & mie_meie) mip_meip <= 0;
                 else if (mip_msip & mie_msie) mip_msip <= 0;
                 else if (mip_mtip & mie_mtie) mip_mtip <= 0;
@@ -657,12 +649,12 @@ always @(posedge clk) begin
         $display("data,csrstage.$.invalidate_i$,b,%b", cache_cntr.invalidate_icache);
     end
 
-    if (valid & (csr_cmd != CSR_X | this_cause_trap)) begin
+    if (valid & (csr_cmd != CSR_X | this_raise_trap)) begin
         $display("data,csrstage.csr_cmd,d,%b", csr_cmd);
         $display("data,csrstage.addr,h,%b", addr);
         $display("data,csrstage.wdata,h,%b", wdata);
         $display("data,csrstage.rdata,h,%b", next_csr_rdata);
-        $display("data,csrstage.csr_is_trap,b,%b", this_cause_trap);
+        $display("data,csrstage.csr_is_trap,b,%b", this_raise_trap);
     end
 end
 `endif
